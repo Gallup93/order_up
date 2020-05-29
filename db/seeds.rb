91e336a1a0ab21d95196618d06a12c0c6121f1d1
Michael Gallup <Gallup93@icloud.com>
2020-05-29 08:56:00 -0600

    Add seeds, Bundle install gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+chef1 = Chef.create(name: "Ronald McDonald")
+chef2 = Chef.create(name: "Samantha Gates")
+
+dish1 = chef1.dishes.create(name: "Nuggies", description: "100% real chicken (30% of the time)")
+dish2 = chef1.dishes.create(name: "McFlurry", description: "The machine is broken")
+dish3 = chef2.dishes.create(name: "Poptart Casserole", description: "World Famous")
+dish2 = chef2.dishes.create(name: "Snail Legs", description: "Hard to find")
